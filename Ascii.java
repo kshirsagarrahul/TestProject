ec77292683b4b5cec55336194fe6c5e00beb5983
Rahul Kshirsagar <122010961+kshirsagarrahul@users.noreply.github.com>
2023-02-18 10:49:51 +0530

    Add files via upload

---
+++ Ascii.java
@@ -0,0 +1,14 @@
+
+
+
+class Ascii
+{
+public static void main(String[]args)
+{
+char c='a';
+int ascii=c;
+int asciiNumber=(int) c;
+System.out.println(ascii);
+System.out.println(asciiNumber);
+}
+}
